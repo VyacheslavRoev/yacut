82b20fe63756eb33e66d169b41c6ed741679b0db
Вячеслав Роев <roev96@yandex.ru>
2023-01-18 15:29:02 +0300

    Добавлены формы, отредактированы шаблоны

--- yacut/views.py
+++ yacut/views.py
@@ -1,7 +1,22 @@
 from . import app, db
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
+from .forms import YacutForm
+from .models import URLMap
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index_view():
-    return render_template('index.html')
+    form = YacutForm()
+    if form.validate_on_submit():
+        urlmap = URLMap(
+            original=form.original_link.data,
+            short=form.custom_id.data
+        )
+        db.session.add(urlmap)
+        db.session.commit()
+        flash(url_for('unique_short', short=form.custom_id.data, _external=True))
 
+    return render_template('main.html', form=form)
+
+@app.route('/<string:short>')
+def unique_short(short):
+    return redirect(URLMap.query.filter_by(short=short))
